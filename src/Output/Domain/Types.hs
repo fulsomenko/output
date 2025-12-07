7c446e073dc917ba0c0f9275d5a863777aa0a5fe
Max Blomstervall <max.blomstervall@gmail.com>
2025-12-07 22:28:44 +0100

    feat(domain): add TypingProgress type for completion tracking

--- src/Output/Domain/Types.hs
+++ src/Output/Domain/Types.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Output.Domain.Types
     ( TOPIK_Level(..)
@@ -8,12 +9,17 @@ module Output.Domain.Types
     , MasteryLevel(..)
     , VocabularyState(..)
     , UserProgress(..)
+      -- * Typing Progress
+    , TypingProgress(..)
+    , emptyTypingProgress
     ) where
 
 import Data.Text (Text)
 import Data.Time (LocalTime)
 import Data.Map (Map)
-import Data.Aeson (FromJSON, ToJSON, FromJSONKey, ToJSONKey)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Aeson (FromJSON(..), ToJSON(..), FromJSONKey, ToJSONKey, object, withObject, (.:), (.=))
 import GHC.Generics (Generic)
 
 -- | TOPIK proficiency levels
@@ -86,3 +92,27 @@ data UserProgress = UserProgress
 
 instance FromJSON UserProgress
 instance ToJSON UserProgress
+
+-- | Progress tracking for typing practice levels
+data TypingProgress = TypingProgress
+    { tpCompletedLevels :: Set Int  -- Set of level numbers completed at least once
+    } deriving (Show, Eq, Generic)
+
+-- | Custom JSON instance for extensible format:
+-- { "typingPractice": { "completedLevels": [11, 12, 13] } }
+instance ToJSON TypingProgress where
+    toJSON progress = object
+        [ "typingPractice" .= object
+            [ "completedLevels" .= Set.toList (tpCompletedLevels progress)
+            ]
+        ]
+
+instance FromJSON TypingProgress where
+    parseJSON = withObject "TypingProgress" $ \v -> do
+        tp <- v .: "typingPractice"
+        levels <- tp .: "completedLevels"
+        pure $ TypingProgress (Set.fromList levels)
+
+-- | Empty typing progress (nothing completed)
+emptyTypingProgress :: TypingProgress
+emptyTypingProgress = TypingProgress Set.empty
