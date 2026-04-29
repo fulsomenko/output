ba26c37afa3a64f58d2ea2f60f21dacb6a94322e
Max Blomstervall <max.blomstervall@gmail.com>
2026-04-29 13:51:55 +0200

    test(srs): strengthen interval-ordering tests and cover out-of-range rating

--- test/Output/Algorithm/SRSSpec.hs
+++ test/Output/Algorithm/SRSSpec.hs
@@ -10,7 +10,7 @@ import Output.Algorithm.SRS
     , SRSAlgorithm(..)
     )
 import Output.Algorithm.SpacedRepetition (defaultSM2)
-import Output.Domain.Types (VocabularyId(..), newVocabularyState)
+import Output.Domain.Types (VocabularyId(..), newVocabularyState, vstCurrentInterval, vstReviewCount)
 
 testTime :: LocalTime
 testTime = LocalTime (fromGregorian 2026 1 1) (TimeOfDay 0 0 0)
@@ -26,21 +26,32 @@ spec = do
             ratingToQuality 3 `shouldBe` Good
         it "maps 4 to Easy" $
             ratingToQuality 4 `shouldBe` Easy
+        it "maps out-of-range (0) to Good" $
+            ratingToQuality 0 `shouldBe` Good
 
     describe "calculateReview (SM-2)" $ do
-        let state = newVocabularyState (VocabularyId 1) testTime
+        let establishedState = (newVocabularyState (VocabularyId 1) testTime)
+                { vstCurrentInterval = 6
+                , vstReviewCount = 2
+                }
 
-        it "Again produces shorter or equal interval than Good" $ do
-            let againResult = calculateReview defaultSM2 state Again testTime
-                goodResult  = calculateReview defaultSM2 state Good  testTime
-            srsNewInterval againResult `shouldSatisfy` (<= srsNewInterval goodResult)
+        it "Again resets interval to 1" $
+            srsNewInterval (calculateReview defaultSM2 establishedState Again testTime)
+                `shouldBe` 1
 
-        it "Easy produces longer or equal interval than Good" $ do
-            let goodResult = calculateReview defaultSM2 state Good testTime
-                easyResult = calculateReview defaultSM2 state Easy testTime
-            srsNewInterval easyResult `shouldSatisfy` (>= srsNewInterval goodResult)
+        it "Again produces strictly shorter interval than Good" $ do
+            let againResult = calculateReview defaultSM2 establishedState Again testTime
+                goodResult  = calculateReview defaultSM2 establishedState Good  testTime
+            srsNewInterval againResult `shouldSatisfy` (< srsNewInterval goodResult)
 
-        it "Hard produces shorter or equal interval than Easy" $ do
-            let hardResult = calculateReview defaultSM2 state Hard testTime
-                easyResult = calculateReview defaultSM2 state Easy testTime
-            srsNewInterval hardResult `shouldSatisfy` (<= srsNewInterval easyResult)
+        it "Easy produces strictly longer interval than Good" $ do
+            let goodResult = calculateReview defaultSM2 establishedState Good testTime
+                easyResult = calculateReview defaultSM2 establishedState Easy testTime
+            srsNewInterval easyResult `shouldSatisfy` (> srsNewInterval goodResult)
+
+        it "Hard falls strictly between Again and Good" $ do
+            let againResult = calculateReview defaultSM2 establishedState Again testTime
+                hardResult  = calculateReview defaultSM2 establishedState Hard  testTime
+                goodResult  = calculateReview defaultSM2 establishedState Good  testTime
+            srsNewInterval hardResult `shouldSatisfy` (> srsNewInterval againResult)
+            srsNewInterval hardResult `shouldSatisfy` (< srsNewInterval goodResult)
