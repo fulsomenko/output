3f235221be22cd302ad2bf7b111d77079ef201a2
Max Blomstervall <max.blomstervall@gmail.com>
2026-05-08 11:18:40 +0200

    refactor(tui): simplify quit confirmation hint text to [Y] Quit  [n] Cancel

--- src/Output/TUI/Draw.hs
+++ src/Output/TUI/Draw.hs
@@ -345,7 +345,7 @@ drawQuitConfirm =
     border $
     padAll 2 $ vBox
         [ txt "Are you sure you want to quit?"
-        , padTop (Pad 1) $ txt "[Enter/y] Quit  [n] Cancel"
+        , padTop (Pad 1) $ txt "[Y] Quit  [n] Cancel"
         ]
 
 -- | Draw typing practice screen with frame
